a79a3ee503e7a8f05ae61c58f29a28810b92394a
Aida <aidagarfami@gmail.com>
2023-07-21 16:48:32 +0200

    show current temp

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-function showTemperature(response) {
+function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = response.data.main.temp;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
 
   let weatherCondition = document.querySelector("#description");
-  weatherCondition.innerHTML = response.data;
-  axios.get(apiurl).then(showTemperature);
+  weatherCondition.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+
+  let wind = document.querySelector("#windSpeed");
+  wind.innerHTML = Math.round(response.data.main.wind.speed);
 }
 
-let city = document.querySelector("#cityInput");
+function searchCity(event) {
+  let apikey = "193326794b34c5f0cbbc22b088ed5be5";
 
-let apikey = "193326794b34c5f0cbbc22b088ed5be5";
+  let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${cityElement}&appid=${apikey}&units=metric`;
 
-let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+  axios.get(apiurl).then(displayTemperature);
+}
+
+let cityElement = document.querySelector("#cityInput");
+
+cityElement.addEventListener("submit", searchCity);

cc746ceceb6783422be688fc8490ffd9bd3dad38
Aida <aidagarfami@gmail.com>
2023-07-21 18:19:17 +0200

    current time

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+function formatDate(timestamp) {
+  let hours = timestamp.getHours();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = timestamp.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -13,6 +39,9 @@ function displayTemperature(response) {
 
   let wind = document.querySelector("#windSpeed");
   wind.innerHTML = Math.round(response.data.main.wind.speed);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 function searchCity(event) {
